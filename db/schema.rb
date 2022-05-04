ec1076fbd17c25b41d6cb99597db03f7b5837354
eva lampin <eva.lampin@hotmail.fr>
2022-05-04 12:07:30 +0200

    Validations done

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_04_092706) do
+ActiveRecord::Schema.define(version: 2022_05_04_094207) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 2022_05_04_092706) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["list_id"], name: "index_bookmarks_on_list_id"
+    t.index ["movie_id", "list_id"], name: "index_bookmarks_on_movie_id_and_list_id", unique: true
     t.index ["movie_id"], name: "index_bookmarks_on_movie_id"
   end
 
